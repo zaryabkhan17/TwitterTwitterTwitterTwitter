c6de2251f5a696a9128ab994ecab0398ccf61002
iamfarooqi <attaurrahmanfarooqi9@gmail.com>
2021-01-30 18:07:51 +0500

    chngeas

---
+++ public/app.js
@@ -0,0 +1,380 @@
+// var url = "http://localhost:5000"
+var url = "https://twitterzaryab.herokuapp.com"
+
+
+var socket = io(url);
+socket.on('connect', function () {
+    console.log("connected")
+});
+
+function tweetsBox(name, email, profilepic, tweetimg, date, tweet) {
+    let box = `
+     <section class="" >
+     <div class="container">
+         <div class="row justify-content-center">
+             <div class="col-md-6   ">
+                 <div class="posts">
+             <div class="row">
+                 <div class="col-md-2 m-0 ">
+                     <img style="border-radius: 100%;" width="50" height="50"
+                         src="${profilepic}"
+                         alt="">
+                 </div>
+                 <div class="col-md-10 ">
+                     <span style="margin:  0; font-weight: bold" >${name}</span>
+                     <span class="float-right">${date}</span>
+                     <p style="margin:  0;">${email}</p>
+                 </div>
+             </div>
+             <div class="row">
+                 <div class="col-md-12">
+                     <p class="mt-1">${tweet}</p>
+                 </div>
+             </div>
+             <div class="row">
+                 <div class="col-md-12">
+                     <img width="100%" src="${tweetimg}" alt="">
+                 </div>
+             </div> 
+             <div class="row">
+                 <div class="col-md-12">
+                     
+                 </div>
+             </div>    
+         </div>
+     </div>
+ </div>
+     </div>
+
+     </div>
+ </section>
+    `
+    return box
+}
+function signup() {
+    axios({
+        method: 'post',
+        url: url + '/signup',
+        data: {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+            phone: document.getElementById('phone').value,
+            gender: document.getElementById('gender').value,
+        },
+        withCredentials: true
+    }).then((response) => {
+        if (response.data.status === 200) {
+            alert(response.data.message)
+            location.href = "./login.html"
+        } else {
+            alert(response.data.message);
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+    return false
+}
+
+//user login request using axios
+function login() {
+    axios({
+        method: 'post',
+        url: url + '/login',
+        data: {
+            email: document.getElementById('lemail').value,
+            password: document.getElementById('lpassword').value,
+        },
+        withCredentials: true
+    }).then((response) => {
+        if (response.data.status === 200) {
+            alert(response.data.message)
+            location.href = "./profile.html"
+        }
+        else {
+            alert(response.data.message)
+        }
+    }, (error) => {
+        console.log(error);
+    });
+    return false
+}
+
+//get profile request using axios
+function getProfile() {
+    axios({
+        method: 'get',
+        url: url + '/profile',
+        credentials: 'include',
+    }).then((response) => {
+        let src = response.data.profile.profilePic
+        let name = response.data.profile.name;
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+        document.getElementById('pName').innerHTML =name
+        if (src) {
+            document.getElementById('profilePic').style.backgroundImage = `url(${src})`;
+            document.getElementById('createPostImg').src = src;
+        }
+        else {
+            document.getElementById('createPostImg').style.backgroundImage = `url(${'./fallback.png'})`;
+            document.getElementById('profilePic').src = './fallback.png';
+        }
+        sessionStorage.setItem('email', response.data.profile.email)
+    }, (error) => {
+        location.href = "./login.html"
+    });
+    return false
+}
+function post() {
+    var fileInput = document.getElementById("customFile");
+    var tweet = document.getElementById('tweet')
+    console.log(fileInput.files[0])
+    if (fileInput.files[0] === undefined) {
+        axios({
+            method: 'post',
+            url: url + '/textTweet',
+            data: {
+                tweet: document.getElementById('tweet').value
+            },
+            withCredentials: true
+        }).then((response) => {
+            console.log(response.data)
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+    else {
+        let formData = new FormData();
+        formData.append("myFile", fileInput.files[0]);
+        formData.append("tweet", tweet.value);
+
+        axios({
+            method: 'post',
+            url: url + "/tweet",
+            data: formData,
+            headers: { 'Content-Type': 'multipart/form-data' }
+        })
+            .then(response => {
+                console.log("response data=> ", response);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+
+    return false;
+
+}
+
+//get all user post or tweet request using axios
+function getTweets() {
+    axios({
+        method: 'get',
+        url: url + '/getTweets',
+        credentials: 'include',
+    }).then((response) => {
+        console.log(response.data.data)
+        let tweets = response.data.data;
+        let html = ""
+        tweets.forEach(element => {
+            var name = element.name.charAt(0).toUpperCase() + element.name.slice(1)
+            if (element.profilePic) {
+                html += tweetsBox(name, element.email,
+                    element.profilePic, element.tweetImg,
+                    new Date(element.createdOn).toLocaleTimeString(), element.tweets)
+            }
+            else {
+                html += tweetsBox(name, element.email,
+                    './fallback.png', element.tweetImg,
+                    new Date(element.createdOn).toLocaleTimeString(), element.tweets)
+            }
+        });
+        document.getElementById('posts').innerHTML = html;
+    }, (error) => {
+        console.log(error.message);
+    });
+    return false
+}
+function myTweets() {
+    axios({
+        method: 'get',
+        url: url + '/myTweets',
+        credentials: 'include',
+    }).then((response) => {
+        console.log(response.data.data)
+        let userTweet = response.data.data
+        let userHtml = ""
+        userTweet.forEach(element => {
+            var name = element.name.charAt(0).toUpperCase() + element.name.slice(1)
+            if (element.profilePic) {
+                userHtml += tweetsBox(name, element.email,
+                    element.profilePic, element.tweetImg,
+                    new Date(element.createdOn).toLocaleTimeString(), element.tweets)
+            }
+            else {
+                userHtml += tweetsBox(element.name, element.email,
+                    './fallback.png', element.tweetImg,
+                    new Date(element.createdOn).toLocaleTimeString(), element.tweets)
+            }
+        });
+        document.getElementById('userPosts').innerHTML = userHtml;
+    }, (error) => {
+        console.log(error.message);
+    });
+
+}
+socket.on('NEW_POST', (newPost) => {
+    console.log(newPost)
+    let tweets = newPost;
+    var name = tweets.name.charAt(0).toUpperCase() + tweets.name.slice(1)
+    document.getElementById('posts').innerHTML += tweetsBox(name, tweets.email,
+        tweets.profilePic, tweets.tweetImg,
+        new Date(tweets.createdOn).toLocaleTimeString(), tweets.tweets)
+
+    document.getElementById('userPosts').innerHTML += tweetsBox(name, tweets.email,
+        tweets.profilePic, tweets.tweetImg,
+        new Date(tweets.createdOn).toLocaleTimeString(), tweets.tweets)
+
+})
+
+//forget password request step1 using axios
+function forgetPassword() {
+    let email = document.getElementById('fEmail').value;
+    localStorage.setItem('email', email)
+    axios({
+        method: 'post',
+        url: url + '/forget-password',
+        data: {
+            email: email,
+        },
+        withCredentials: true
+    }).then((response) => {
+        if (response.data.status === 200) {
+            alert(response.data.message)
+            location.href = "./forget2.html"
+        }
+        else {
+            alert(response.data.message)
+        }
+    }, (error) => {
+        console.log(error);
+    });
+    return false
+}
+
+//forget password request step2 using axios
+function forgetPassword2() {
+    let getEmail = localStorage.getItem('email')
+    axios({
+        method: 'post',
+        url: url + '/forget-password-2',
+        data: {
+            email: getEmail,
+            newPassword: document.getElementById('newPassword').value,
+            otp: document.getElementById('otp').value,
+        },
+        withCredentials: true
+    }).then((response) => {
+        if (response.data.status === 200) {
+            alert(response.data.message)
+            location.href = "./login.html"
+        }
+        else {
+            alert(response.data.message)
+        }
+    }, (error) => {
+        console.log(error);
+    });
+    return false
+}
+
+//profile logout request using axios
+function logout() {
+    axios({
+        method: 'post',
+        url: url + '/logout',
+    }).then((response) => {
+        location.href = "./login.html"
+    }, (error) => {
+        console.log(error);
+    });
+    return false
+}
+
+//display homepage using display none or block property
+document.getElementById('profile').style.display = "none"
+function showHome() {
+    document.getElementById('profile').style.display = "none"
+    document.getElementById('home').style.display = "block"
+
+}
+
+//display profile page using display none or block property
+function showProfile() {
+    document.getElementById('home').style.display = "none"
+    document.getElementById('profile').style.display = "block"
+}
+
+function showUsers() {
+    document.getElementById('home').style.display = "none"
+    document.getElementById('profile').style.display = "none"
+
+}
+function upload() {
+
+    var fileInput = document.getElementById("fileToUpload");
+    document.getElementById("Upload").innerHTML = "Uploading!";
+    console.log("fileInput: ", fileInput);
+    console.log("fileInput: ", fileInput.files[0]);
+    // document.getElementById('profilePic').style.backgroundImage = `url(${fileInput.files[0]})`;
+
+    let formData = new FormData();
+    formData.append("myFile", fileInput.files[0]);
+    formData.append("email", sessionStorage.getItem('email'));
+
+    axios({
+        method: 'post',
+        url: url + "/upload",
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' }
+    })
+        .then(res => {
+            var userData = res
+            console.log("response data=> ", res.data);
+            console.log(`upload Success` + userData.toString());
+            document.getElementById("profilePic").style.backgroundImage = `url(${res.data.url})`
+            document.getElementById("Upload").innerHTML = res.data.message;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    return false;
+
+}
+function preview() {
+    const preview = document.querySelector('img')
+    const file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        // convert image file to base64 string
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
---
+++ server.js
@@ -0,0 +1,289 @@
+var SERVER_SECRET = process.env.SECRET || "1434";
+// var SERVICE_ACCOUNT = JSON.parse(process.env.SERVICE_ACCOUNT)
+const PORT = process.env.PORT || 5000;
+
+var express = require("express");
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cors = require("cors");
+var morgan = require("morgan");
+var path = require("path")
+var jwt = require('jsonwebtoken')
+var { userModel, tweetModel } = require('./dbconn/modules');
+var app = express();
+var authRoutes = require('./routes/auth')
+var http = require("http");
+var socketIO = require("socket.io");
+var server = http.createServer(app);
+var io = socketIO(server);
+const fs = require('fs')
+const multer = require('multer')
+
+io.on("connection", () => {
+    console.log("chl gya");
+})
+
+const storage = multer.diskStorage({ // https://www.npmjs.com/package/multer#diskstorage
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+        cb(null, `${new Date().getTime()}-${file.filename}.${file.mimetype.split("/")[1]}`)
+    }
+})
+var upload = multer({ storage: storage })
+
+const admin = require("firebase-admin");
+// https://firebase.google.com/docs/storage/admin/start
+var SERVICE_ACCOUNT = JSON.parse(process.env.SERVICE_ACCOUNT)
+
+admin.initializeApp({
+    credential: admin.credential.cert(SERVICE_ACCOUNT),
+    databaseURL: process.env.databaseURL
+});
+const bucket = admin.storage().bucket(process.env.bucket);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+app.use(morgan('dev'));
+app.use("/", express.static(path.resolve(path.join(__dirname, "public"))))
+
+app.use('/', authRoutes);
+app.use(function (req, res, next) {
+    if (!req.cookies.jToken) {
+        res.status(401).send("include http-only credentials with every request")
+        return;
+    }
+    jwt.verify(req.cookies.jToken, SERVER_SECRET, function (err, decodedData) {
+        if (!err) {
+
+            const issueDate = decodedData.iat * 1000;
+            const nowDate = new Date().getTime();
+            const diff = nowDate - issueDate;
+
+            if (diff > 300000) {
+                res.status(401).send("token expired")
+            } else {
+                var token = jwt.sign({
+                    id: decodedData.id,
+                    name: decodedData.name,
+                    email: decodedData.email,
+                }, SERVER_SECRET)
+                res.cookie('jToken', token, {
+                    maxAge: 86400000,
+                    httpOnly: true
+                });
+                req.body.jToken = decodedData
+                req.headers.jToken = decodedData
+                next();
+            }
+        } else {
+            res.status(401).send("invalid token")
+        }
+    });
+})
+
+app.get("/profile", (req, res, next) => {
+
+    console.log(req.body)
+
+    userModel.findById(req.body.jToken.id, 'name email phone gender createdOn profilePic',
+        function (err, doc) {
+            if (!err) {
+                res.send({
+                    status: 200,
+                    profile: doc
+                })
+
+            } else {
+                res.status(500).send({
+                    message: "server error"
+                })
+            }
+        })
+})
+app.post("/tweet", upload.any(), (req, res, next) => {
+
+    console.log("req.body: ", req.body);
+    bucket.upload(
+        req.files[0].path,
+        function (err, file, apiResponse) {
+            if (!err) {
+                file.getSignedUrl({
+                    action: 'read',
+                    expires: '03-09-2491'
+                }).then((urlData, err) => {
+                    if (!err) {
+                        console.log("public downloadable url: ", urlData[0])
+                        userModel.findById(req.headers.jToken.id, 'name profilePic email', (err, user) => {
+                            console.log("user =======>", user.email)
+                            if (!err) {
+                                tweetModel.create({
+                                    "name": user.name,
+                                    "tweets": req.body.tweet,
+                                    "profilePic": user.profilePic,
+                                    "tweetImg": urlData[0],
+                                    "email": user.email
+                                }).then((data) => {
+                                    console.log(data)
+                                    res.send({
+                                        status: 200,
+                                        message: "Post created",
+                                        data: data
+                                    })
+                                    console.log(data)
+                                    io.emit("NEW_POST", data)
+
+                                }).catch(() => {
+                                    console.log(err);
+                                    res.status(500).send({
+                                        message: "user create error, " + err
+                                    })
+                                })
+                            }
+                            else {
+                                res.send("err")
+                            }
+                        })
+                        try {
+                            fs.unlinkSync(req.files[0].path)
+                        } catch (err) {
+                            console.error(err)
+                        }
+                    }
+                })
+            } else {
+                console.log("err: ", err)
+                res.status(500).send();
+            }
+        });
+})
+
+app.post('/textTweet', (req, res, next) => {
+    if (!req.body.tweet) {
+        res.status(403).send({
+            message: "please provide tweet"
+        })
+    }
+    userModel.findOne({ email: req.body.jToken.email }, (err, user) => {
+        console.log("user =======>", user.email)
+        if (!err) {
+            tweetModel.create({
+                "name": user.name,
+                "email": user.email,
+                "tweets": req.body.tweet,
+                "profilePic": user.profilePic,
+            }).then((data) => {
+                console.log(data)
+                res.send({
+                    status: 200,
+                    message: "Post created",
+                    data: data
+                })
+                io.emit("NEW_POST", data)
+            }).catch(() => {
+                console.log(err);
+                res.status(500).send({
+                    message: "user create error, " + err
+                })
+            })
+        }
+        else {
+            console.log(err)
+        }
+    })
+})
+
+app.get('/getTweets', (req, res, next) => {
+
+    tweetModel.find({}, (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send({ data: data });
+        }
+    })
+})
+app.get('/myTweets', (req, res, next) => {
+
+    userModel.findOne({ email: req.body.jToken.email }, (err, user) => {
+        if (!err) {
+            tweetModel.find({ email: req.body.jToken.email }, (err, data) => {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    res.send({ data: data });
+                }
+            })
+        }
+        else {
+            console.log(err)
+        }
+    })
+})
+app.post("/upload", upload.any(), (req, res, next) => {
+
+    console.log("req.body: ", req.body);
+    bucket.upload(
+        req.files[0].path,
+        function (err, file, apiResponse) {
+            if (!err) {
+                file.getSignedUrl({
+                    action: 'read',
+                    expires: '03-09-2491'
+                }).then((urlData, err) => {
+                    if (!err) {
+                        console.log("public downloadable url: ", urlData[0])
+                        userModel.findOne({ email: req.body.email }, (err, user) => {
+                            console.log(user)
+                            if (!err) {
+                                tweetModel.updateMany({ name: req.headers.jToken.name }, { profilePic: urlData[0] }, (err, tweetModel) => {
+                                    console.log(tweetModel)
+                                    if (!err) {
+                                        console.log("update");
+                                    }
+                                });
+                                user.update({ profilePic: urlData[0] }, (err, updatedProfile) => {
+                                    if (!err) {
+                                        res.status(200).send({
+                                            message: "succesfully uploaded",
+                                            url: urlData[0],
+                                        })
+                                    }
+                                    else {
+                                        res.status(500).send({
+                                            message: "an error occured" + err,
+                                        })
+                                    }
+
+                                })
+                            }
+                            else {
+                                res.send({
+                                    message: "error"
+                                });
+                            }
+                        })
+                        try {
+                            fs.unlinkSync(req.files[0].path)
+                        } catch (err) {
+                            console.error(err)
+                        }
+                    }
+                })
+            } else {
+                console.log("err: ", err)
+                res.status(500).send();
+            }
+        });
+})
+
+server.listen(PORT, () => {
+    console.log("server is running on: ", PORT);
+})
